0165d6b4cc70c07c84b586ef499e45f319ac0fe3
Harry von Borstel <re+git@blueshell.com>
2017-04-15 16:52:15 +0200

    Update WindowsAPICodePack

--- Controls/MockFileSystem.cs
+++ Controls/MockFileSystem.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.WindowsAPICodePack.Shell;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Controls
+﻿namespace Controls
 {
     public class MockFileSystem
     {
