311d1029b0c4f959220b788b51c7d0d91ea8ddce
Harry von Borstel <re+git@blueshell.com>
2022-01-01 15:05:38 +0100

    Migrated to SDK-style projects

--- TVM/Properties/AssemblyInfo.cs
+++ TVM/Properties/AssemblyInfo.cs
@@ -7,18 +7,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Typesafe View Model")]
-[assembly: AssemblyDescription("Class library using typesafe binding for ViewModels")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("blueshell Software Engineering Harry von Borstel")]
-[assembly: AssemblyProduct("Typesafe View Model")]
-[assembly: AssemblyCopyright("Copyright ©  2012 by blueshell Software Engineering Harry von Borstel")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -26,17 +14,4 @@ using System;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9af69c49-9c0a-468c-8ada-0a6bf3796c63")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
