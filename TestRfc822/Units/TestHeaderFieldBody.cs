a03b3ef3b86aa922b09055f0da23f2af432b0c85
Harry von Borstel <re+git@blueshell.com>
2024-03-28 13:44:46 +0100

    Fix unit tests

--- TestRfc822/Units/TestHeaderFieldBody.cs
+++ TestRfc822/Units/TestHeaderFieldBody.cs
@@ -1,18 +1,19 @@
 ﻿using blueshell.rfc822;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Text;
+using TestUtils;
 
 namespace TestRfc822
 {
 	[TestClass]
 	public class TestHeaderFieldBody
 	{
-        public TestHeaderFieldBody()
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        }
+		public TestHeaderFieldBody()
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
 
-        [TestMethod]
+		[TestMethod]
 		public void TestHeaderFieldEmpty()
 		{
 			var hf = new HeaderFieldBody();
@@ -30,34 +31,34 @@ namespace TestRfc822
 		public void TestHeaderFieldSimple()
 		{
 			var hf = new HeaderFieldBody("This is a simple body");
-			Assert.AreEqual("Name:\tThis is a simple body\r\n" ,hf.ToString("Name"));
+			Assert.AreEqual("Name:\tThis is a simple body\r\n", hf.ToString("Name"));
 		}
 
-        [TestMethod]
-        public void TestHeaderFieldSpecial()
-        // Tests RFC2047 encoding https://www.ietf.org/rfc/rfc2047.txt
-        {
-            var naturalText = "German: ÄÖÜäöüß French: ÁÀÉÈÔÊÂáàéèôêâÆæÔôÇç Spanish: Ññ¡¿";
-            var encodedText = 
-                    "=?iso-8859-15?Q?German:_=C4=D6=DC=E4=F6=FC=DF_French:_=C1=C0=C9=C8=D4=CA?=\r\n"
-                    + " =?iso-8859-15?Q?=C2=E1=E0=E9=E8=F4=EA=E2=C6=E6=D4=F4=C7=E7_Spanish:_=D1?=\r\n"
-                    + " =?iso-8859-15?Q?=F1=A1=BF?=\r\n";
-            var hf = new HeaderFieldBody(naturalText);
-            Assert.AreEqual(
-                "Name:\r\n\t" + encodedText,
-                hf.ToString("Name"));
+		[TestMethod]
+		public void TestHeaderFieldSpecial()
+		// Tests RFC2047 encoding https://www.ietf.org/rfc/rfc2047.txt
+		{
+			var naturalText = "German: ÄÖÜäöüß French: ÁÀÉÈÔÊÂáàéèôêâÆæÔôÇç Spanish: Ññ¡¿";
+			var encodedText =
+					"=?iso-8859-15?Q?German:_=C4=D6=DC=E4=F6=FC=DF_French:_=C1=C0=C9=C8=D4=CA?=\r\n"
+					+ " =?iso-8859-15?Q?=C2=E1=E0=E9=E8=F4=EA=E2=C6=E6=D4=F4=C7=E7_Spanish:_=D1?=\r\n"
+					+ " =?iso-8859-15?Q?=F1=A1=BF?=\r\n";
+			var hf = new HeaderFieldBody(naturalText);
+			AssertText.StringsAreEqual(
+				"Name:\r\n\t" + encodedText,
+				hf.ToString("Name"));
 
-            var hf2 = new HeaderFieldBody(encodedText);
-            Assert.AreEqual(
-                "Name2:\r\n\t" + encodedText,
-                hf2.ToString("Name2"));
-        }
+			var hf2 = new HeaderFieldBody(encodedText);
+			AssertText.StringsAreEqual(
+				"Name2:\r\n\t" + encodedText,
+				hf2.ToString("Name2"));
+		}
 
-        [TestMethod]
+		[TestMethod]
 		public void TestHeaderFieldPreserveFolding()
 		{
 			var hf = new HeaderFieldBody("This is a\r\n\tfolded body");
-			Assert.AreEqual("Name:\tThis is a\r\n\tfolded body\r\n", hf.ToString("Name"));
+			AssertText.StringsAreEqual("Name:\tThis is a\r\n\tfolded body\r\n", hf.ToString("Name"));
 		}
 
 		[TestMethod]
@@ -68,42 +69,42 @@ namespace TestRfc822
 			Assert.AreEqual("This-really-long-field-name-counts-for-the-total-length-too:\tThis is a long\r\n body that needs some folding in order to prevent lines longer than 78+2\r\n characters.\r\n", act);
 		}
 
-        [TestMethod]
-        public void TestHeaderFieldEncodeFold()
-        {
-            var hf = new HeaderFieldBody(
-            "ThisIsAReallyVeryLongLineThatNeedsToHaveSomeFoldingButThatHasNoWhitespaceToLetTheFoldHappenWithoutInterruptingTheWord");
-            var act = hf.ToString("Field-name");
-            Assert.AreEqual(
-                "Field-name:\r\n" + "\t=?iso-8859-15?Q?ThisIsAReallyVeryLongLineThatNeedsToHaveSomeFoldingBut?=\r\n"
-                    + " =?iso-8859-15?Q?ThatHasNoWhitespaceToLetTheFoldHappenWithoutInterrupti?=\r\n"
-                    + " =?iso-8859-15?Q?ngTheWord?=\r\n",
-                act);
-        }
+		[TestMethod]
+		public void TestHeaderFieldEncodeFold()
+		{
+			var hf = new HeaderFieldBody(
+			"ThisIsAReallyVeryLongLineThatNeedsToHaveSomeFoldingButThatHasNoWhitespaceToLetTheFoldHappenWithoutInterruptingTheWord");
+			var act = hf.ToString("Field-name");
+			Assert.AreEqual(
+				"Field-name:\r\n" + "\t=?iso-8859-15?Q?ThisIsAReallyVeryLongLineThatNeedsToHaveSomeFoldingBut?=\r\n"
+					+ " =?iso-8859-15?Q?ThatHasNoWhitespaceToLetTheFoldHappenWithoutInterrupti?=\r\n"
+					+ " =?iso-8859-15?Q?ngTheWord?=\r\n",
+				act);
+		}
 
-        [TestMethod]
-        public void TestHeaderFieldDecode1()
-        {
-            var e1 = false;
-            var act = HeaderFieldBody.DecodeWord("=?iso-8859-15?Q?De:_=C4=D6=DC=E4=F6=FC=DF_Fr:_=C1=C0=C9=C8=D4=CA?=", ref e1);
+		[TestMethod]
+		public void TestHeaderFieldDecode1()
+		{
+			var e1 = false;
+			var act = HeaderFieldBody.DecodeWord("=?iso-8859-15?Q?De:_=C4=D6=DC=E4=F6=FC=DF_Fr:_=C1=C0=C9=C8=D4=CA?=", ref e1);
 
-            Assert.AreEqual(
-                "De: ÄÖÜäöüß Fr: ÁÀÉÈÔÊ",
-                act
-                );
-        }
+			Assert.AreEqual(
+				"De: ÄÖÜäöüß Fr: ÁÀÉÈÔÊ",
+				act
+				);
+		}
 
-        [TestMethod]
-        public void TestHeaderFieldDecode2()
-        {
-            var e1 = false;
-            var act = HeaderFieldBody.DecodeWord("=?iso-8859-15?Q?Spec:_=A4=A6=A8=B4=B8=BC=BD=BE?=",
-                ref e1);
+		[TestMethod]
+		public void TestHeaderFieldDecode2()
+		{
+			var e1 = false;
+			var act = HeaderFieldBody.DecodeWord("=?iso-8859-15?Q?Spec:_=A4=A6=A8=B4=B8=BC=BD=BE?=",
+				ref e1);
 
-            Assert.AreEqual(
-                "Spec: €ŠšŽžŒœŸ",
-                act
-                );
-        }
-    }
+			Assert.AreEqual(
+				"Spec: €ŠšŽžŒœŸ",
+				act
+				);
+		}
+	}
 }
--- rfc822/HeaderFieldBody.cs
+++ rfc822/HeaderFieldBody.cs
@@ -30,180 +30,191 @@ namespace blueshell.rfc822
 	/// The body (i.e. value) of a header field
 	/// </summary>
 	/// <remarks><see cref="http://tools.ietf.org/html/rfc822#section-3.2"/></remarks>
-    public class HeaderFieldBody : IHeaderFieldBody
-    {
-        private readonly string[] content;
-        public HeaderFieldBody(params string[] contentItems)
-        {
-            var decodedItems = new List<string>();
-            if (contentItems != null)
-                foreach (var contentItem in contentItems)
-                {
-                    decodedItems.Add(DecodeWords(contentItem));
-                }
-            this.content = decodedItems.ToArray();
-        }
+	public class HeaderFieldBody : IHeaderFieldBody
+	{
+		private readonly string[] content;
+		public HeaderFieldBody(params string[] contentItems)
+		{
+			var decodedItems = new List<string>();
+			if (contentItems != null)
+				foreach (var contentItem in contentItems)
+				{
+					decodedItems.Add(DecodeWords(contentItem));
+				}
+			this.content = decodedItems.ToArray();
+		}
 
-        private string DecodeWords(string contentItem)
-        // RFC2047 decoding https://www.ietf.org/rfc/rfc2047.txt
-        {
-            var result = "";
-            var delim = "";
-            var lastWordWasEncoded = false;
-            foreach (var word in contentItem.Split(new[] { ' ', '\t' }))
-            {
-                var e1 = lastWordWasEncoded;
-                var w = DecodeWord(word.Trim(), ref lastWordWasEncoded);
-                var e2 = lastWordWasEncoded;
-                var d =
-                    e1 && e2
-                        ? ""
-                        : delim;
-                result += d + w;
-                delim = " ";
-            }
-            return result;
-        }
+		private string DecodeWords(string contentItem)
+		// RFC2047 decoding https://www.ietf.org/rfc/rfc2047.txt
+		{
+			var result = "";
+			var delim = "";
+			var lastWordWasEncoded = false;
+			foreach (var word in contentItem.Split(new[] { ' ' }))
+			{
+				foreach (var word2 in word.Split(new[] { '\t' }))
+				{
+					var e1 = lastWordWasEncoded;
+					var w = DecodeWord(word2, ref lastWordWasEncoded);
+					var e2 = lastWordWasEncoded;
+					var d =
+						e1 && e2
+							? ""
+							: delim;
+					result += d + w;
+					delim = "\t";
+				}
+				delim = " ";
+			}
+			return result;
+		}
 
-        internal static string DecodeWord(string word, ref bool wordWasEncoded)
-        {
-            var lastWordWasEncoded = wordWasEncoded;
-            wordWasEncoded = false;
-            if (word.Length > 75 || !word.StartsWith("=?") || !word.EndsWith("?="))
-                return word;
-            var parts = word.Split('?');
-            if (parts.Length != 5 || parts[0] != "=" || parts[4] != "=")
-                return word;
-            switch(parts[2])
-            {
-                case "Q":
-                    break;
-                case "B":
-                    // sorry, maybe later...
-                    return word;
+		internal static string DecodeWord(string word, ref bool wordWasEncoded)
+		{
+			var lastWordWasEncoded = wordWasEncoded;
+			wordWasEncoded = false;
+			var encodedWord = word.Trim();
+			if (encodedWord.Length > 75)
+				return word;
+			var parts = encodedWord.Split('?');
+			if (parts.Length != 5 || parts[0] != "=" || parts[4] != "=")
+				return word;
+			switch (parts[2])
+			{
+				case "Q":
+					break;
+				case "B":
+					// sorry, maybe later...
+					return word;
 
-                default:
-                    return word;
-            }
+				default:
+					return word;
+			}
 
-            try
-            {
-                var encoding = Encoding.GetEncoding(parts[1]);
-                var data = parts[3];
-                var result = "";
-                for (int i = 0; i < data.Length; i++)
-                {
-                    var c = data[i];
-                    switch (c)
-                    {
-                        case '=':
-                            if (!Regex.IsMatch(data.Substring(i + 1), "^[0-9A-Fa-f]{2}"))
-                                return word;
-                            var b = byte.Parse("00" + data.Substring(i + 1, 2), NumberStyles.AllowHexSpecifier);
-                            result += encoding.GetString(new[] { b });
-                            i += 2;
-                            break;
+			try
+			{
+				var encoding = Encoding.GetEncoding(parts[1]);
+				var data = parts[3];
+				var result = "";
+				for (int i = 0; i < data.Length; i++)
+				{
+					var c = data[i];
+					switch (c)
+					{
+						case '=':
+							if (!Regex.IsMatch(data.Substring(i + 1), "^[0-9A-Fa-f]{2}"))
+								return word;
+							var b = byte.Parse("00" + data.Substring(i + 1, 2), NumberStyles.AllowHexSpecifier);
+							result += encoding.GetString(new[] { b });
+							i += 2;
+							break;
 
-                        case '_':
-                            result += " ";
-                            break;
+						case '_':
+							result += " ";
+							break;
 
-                        default:
-                            result += c;
-                            break;
-                    }
-                }
-                wordWasEncoded = true;
-                return result;
-            }
-            catch
-            {
-                return word;
-            }
-        }
+						default:
+							result += c;
+							break;
+					}
+				}
+				wordWasEncoded = true;
+				return result;
+			}
+			catch
+			{
+				return word;
+			}
+		}
 
-        /// <summary>
-        /// Returns the content string lines of the header field body
-        /// </summary>
-        /// <returns>The content string lines of the header field. If there is no content, null is returned.</returns>
-        public override string ToString()
-        {
-            return
-                content == null || content.Length == 0
-                    ? ""
-                    : content.Aggregate((total, next) => total + "\r\n" + next);
-        }
+		/// <summary>
+		/// Returns the content string lines of the header field body
+		/// </summary>
+		/// <returns>The content string lines of the header field. If there is no content, null is returned.</returns>
+		public override string ToString()
+		{
+			return
+				content == null || content.Length == 0
+					? ""
+					: content.Aggregate((total, next) => total + "\r\n" + next);
+		}
 
-        /// <summary>
-        /// Returns all lines of the header field
-        /// </summary>
-        /// <param name="fieldName">Name of the header field</param>
-        /// <returns>All lines of the header field, consisting of field-name, colon, field-body and trailing CRLF. Folding is applied, thus no line exceeds the 78+2 chars limit. Each occurence of the header field yiels a CRLF-terminated line in RFC 822 format. . If there is no content, null is returned.</returns>
-        public string ToString(string fieldName)
-        {
-            return
-                content == null || content.Length == 0
-                    ? null
-                    : content.Aggregate(
-                    "",
-                    (total, next) =>
-                        total +
-                        (string.Format("{0}:\t{1}\r\n", fieldName, EncodeAsWords(next))).Fold());
-        }
+		/// <summary>
+		/// Returns all lines of the header field
+		/// </summary>
+		/// <param name="fieldName">Name of the header field</param>
+		/// <returns>All lines of the header field, consisting of field-name, colon, field-body and trailing CRLF. Folding is applied, thus no line exceeds the 78+2 chars limit. Each occurence of the header field yiels a CRLF-terminated line in RFC 822 format. . If there is no content, null is returned.</returns>
+		public string ToString(string fieldName)
+		{
+			return
+				content == null || content.Length == 0
+					? null
+					: content.Aggregate(
+					"",
+					(total, next) =>
+						total +
+						(string.Format("{0}:\t{1}\r\n", fieldName, EncodeAsWords(next))).Fold());
+		}
 
 
-        private static string EncodeAsWords(string s)
-            // RFC2047 encoding https://www.ietf.org/rfc/rfc2047.txt
-        {
-            if (!(s.Any(c => c > '~' || c < ' ' ) || NeedsEncodingForFolding(s)))
-                return s;
-            var encoding = Encoding.GetEncoding("iso-8859-15");
-            string word = $"=?{encoding.BodyName}?Q?";
-            string result="";
-            const int maxword = 75; // the max size allowed for encoded-word
-            const int maxlenchar = 3;   // The max length of an encoded char
-            const int lenpostfix = 2; // The length of "?="
-            foreach (var c in s)
-            {
-                if (word.Length >= maxword - maxlenchar - lenpostfix)
-                {
-                    // fold it (i.e. split in order to make it foldable)
-                    result = $"{result}{word}?= ";
-                    word = $"=?{encoding.BodyName}?Q?";
-                }
-                switch (c)
-                {
-                    case ' ':
-                        word = word + "_";
-                        break;
+		private static string EncodeAsWords(string s)
+		// RFC2047 encoding https://www.ietf.org/rfc/rfc2047.txt
+		{
+			if (!(HasSpecialChar(s) || NeedsEncodingForFolding(s)))
+				return s;
+			var encoding = Encoding.GetEncoding("iso-8859-15");
+			string word = $"=?{encoding.BodyName}?Q?";
+			string result = "";
+			const int maxword = 75; // the max size allowed for encoded-word
+			const int maxlenchar = 3;   // The max length of an encoded char
+			const int lenpostfix = 2; // The length of "?="
+			foreach (var c in s)
+			{
+				if (word.Length >= maxword - maxlenchar - lenpostfix)
+				{
+					// fold it (i.e. split in order to make it foldable)
+					result = $"{result}{word}?= ";
+					word = $"=?{encoding.BodyName}?Q?";
+				}
+				switch (c)
+				{
+					case ' ':
+						word = word + "_";
+						break;
 
-                    case '=':
-                    case '?':
-                    case '\t':
-                    case '_':
-                        var bytes = encoding.GetBytes(new char[] { c });
-                        foreach (var b in bytes)
-                        {
-                            word = word + $"={b:X2}";
-                        }
-                        break;
+					case '=':
+					case '?':
+					case '\t':
+					case '_':
+						var bytes = encoding.GetBytes(new char[] { c });
+						foreach (var b in bytes)
+						{
+							word = word + $"={b:X2}";
+						}
+						break;
 
-                    default:
-                        if (c > '~')
-                            goto case '=';
-                        word = word + c;
-                        break;
-                }
-            }
-            return $"{result}{word}?=";
-        }
+					default:
+						if (c > '~')
+							goto case '=';
+						word = word + c;
+						break;
+				}
+			}
+			return $"{result}{word}?=";
 
-        private static bool NeedsEncodingForFolding(string s)
-        {
-            if (s.Length <= 75)
-                return false;
-            var a = s.Split(' ', '\t');
-            return a.Any(s1 => s1.Length > 75);
-        }
-    }
+			static bool HasSpecialChar(string s)
+			{
+				return s.Any(c => c > '~' || (c < ' ' && c != '\r' && c != '\n' && c != '\t'));
+			}
+
+			static bool NeedsEncodingForFolding(string s)
+			{
+				if (s.Length <= 75)
+					return false;
+				var a = s.Split(' ', '\t');
+				return a.Any(s1 => s1.Length > 75);
+			}
+		}
+
+	}
 }
